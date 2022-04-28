63624a21eb24de2abe819ea07aa897d85514f34a
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-04-28 23:40:29 +0300

    Creating and rendering gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const refs = {
+  divGallery: document.querySelector(".gallery"),
+};
+//======== Creating and rendering gallery ==========
+function markupGallery(galleryItems) {
+  galleryItems.map((galleryItem) => {
+    refs.divGallery.insertAdjacentHTML(
+      "beforeend",
+      markupElementGallery(galleryItem)
+    );
+  });
+}
+function markupElementGallery({ preview, original, description }) {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+}
+markupGallery(galleryItems);
 
 console.log(galleryItems);
