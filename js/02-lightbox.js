a012a7b6ed010e99396b02e1011740aa0659b029
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-05-05 00:00:31 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,7 +4,7 @@ import { galleryItems } from "./gallery-items.js";
 const refs = {
   divGallery: document.querySelector(".gallery"),
 };
-//======== Creating and rendering gallery ==========
+//===============================================
 function markupGallery() {
   galleryItems.map((galleryItem) => {
     refs.divGallery.insertAdjacentHTML(
@@ -18,22 +18,16 @@ function markupElementGallery({ preview, original, description }) {
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>`;
 }
-markupGallery();
-//======== Oupen modal window with img ==========
-refs.divGallery.addEventListener("click", (event) => {
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-  event.preventDefault();
-  oupenModalWindow();
-});
-
+//===============================================
 function oupenModalWindow() {
   let gallery = new SimpleLightbox(".gallery a", {
     captionsData: "alt",
-    animationSpeed: 250,
+    captionDelay: 250,
   });
-  gallery.on("show.simplelightbox");
+  gallery.refresh();
 }
+
+markupGallery(); //Creating and rendering gallery
+oupenModalWindow(); //Oupen modal window with img
 
 console.log(galleryItems);
