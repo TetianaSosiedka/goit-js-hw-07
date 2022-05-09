94a98db1269f31162e5fc782f9c7665704e395fa
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-05-09 22:35:08 +0300

    added li in  lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -14,9 +14,9 @@ function markupGallery() {
   });
 }
 function markupElementGallery({ preview, original, description }) {
-  return `<a class="gallery__item" href="${original}">
+  return `<li><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
+</a></li>`;
 }
 //===============================================
 function oupenModalWindow() {
